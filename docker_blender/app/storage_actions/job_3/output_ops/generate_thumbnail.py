1006985424084ac71ef8a0ba834cb881fe1f5bd2
jer-nc <rejdev24@gmail.com>
2024-05-01 17:43:44 +0200

    Refactor Dockerfile to install imagemagick and update generate_thumbnail.py

--- docker_blender/app/storage_actions/job_3/output_ops/generate_thumbnail.py
+++ docker_blender/app/storage_actions/job_3/output_ops/generate_thumbnail.py
@@ -1,5 +1,19 @@
 import os
 from PIL import Image
+from wand.image import Image as WandImage
+
+
+def convert_to_png(image_path):
+    # Nombre del archivo de salida
+    output_path = os.path.splitext(image_path)[0] + '.png'
+    # Abrir la imagen con wand
+    with WandImage(filename=image_path) as img:
+        # Convertir a PNG
+        img.format = 'png'
+        img.save(filename=output_path)
+
+    return output_path
+
 
 def generate_thumbnail(render_output_path, thumbnail_output_path):
     # Crear thumbnail de la primera imagen que haya en output, guardarlo en la misma ruta de files (efs project path env) como: _thumbnail.png
@@ -8,7 +22,7 @@ def generate_thumbnail(render_output_path, thumbnail_output_path):
     image_path = None
     for root, dirs, files in os.walk(render_output_path):
         for file in files:
-            if file.endswith(('.bmp', '.iris', '.png', '.jpg', '.jpeg', '.jp2', '.tga', '.dpx', '.exr', '.hdr', '.tif', 'tiff','.webp')):
+            if file.endswith(('.bmp', '.iris', '.png', '.jpg', '.jpeg', '.jp2', '.tga', '.dpx', '.exr', '.hdr', '.tif', 'tiff', '.webp')):
                 image_path = os.path.join(root, file)
                 break
         if image_path:
@@ -17,7 +31,16 @@ def generate_thumbnail(render_output_path, thumbnail_output_path):
         raise FileNotFoundError(f"No image found in {render_output_path}")
 
     # Abrir la imagen y crear el thumbnail
-    image = Image.open(image_path)
+    try:
+        if image_path.lower().endswith(('.bmp', '.iris', '.jpg', '.jpeg', '.jp2', '.tga', '.dpx', '.exr', '.hdr', '.tif', 'tiff', '.webp')):
+            # Convertir a PNG utilizando ImageMagick (wand)
+            png_path = convert_to_png(image_path)
+            # Abrir la imagen convertida
+            image = Image.open(png_path)
+        else:
+            image = Image.open(image_path)
+    except Exception as e:
+        raise RuntimeError(f"Failed to open image: {e}")
 
     # Calcular las nuevas dimensiones manteniendo el aspect ratio
     width, height = image.size

634d663f3a944cf2a15e1dcb90e94d59aed859d7
jer-nc <rejdev24@gmail.com>
2024-05-06 16:42:32 +0200

    Refactor configure_ses/batch_details.py and configure_ses/presigned_urls.py

--- docker_blender/app/storage_actions/job_3/configure_ses/presigned_urls.py
+++ docker_blender/app/storage_actions/job_3/configure_ses/presigned_urls.py
@@ -7,89 +7,33 @@ import boto3.session
 bucket_name = os.environ.get('BUCKET_NAME')
 bucket_key = os.environ.get('BUCKET_KEY')
 
+# review args if needed!!!
 
 def generate_presigned_urls(region):
     try:
-        # Crear un cliente de IAM
-        iam_client = boto3.client('iam', region_name=region)
+        session = boto3.session.Session(region_name=region)
+        
+        s3_client = session.client('s3', config=Config(signature_version='s3v4'))
 
-        # Nombre del rol que deseas asumir
-        role_name = 'RoleForPresharedURL'
-
-        # Verificar si el rol existe
-        try:
-            iam_client.get_role(RoleName=role_name)
-        except iam_client.exceptions.NoSuchEntityException:
-            print(f"El rol '{role_name}' no existe. Creando rol...")
-
-            # Crear el rol
-            iam_client.create_role(
-                RoleName=role_name,
-                AssumeRolePolicyDocument='''{
-                  "Version": "2012-10-17",
-                  "Statement": [{
-                    "Effect": "Allow",
-                    "Principal": {
-                      "Service": "sts.amazonaws.com"
-                    },
-                    "Action": "sts:AssumeRole"
-                  }]
-                }'''
-            )
-            print(f"Rol '{role_name}' creado correctamente.")
-
-        # Crear un cliente de STS
-        sts_client = boto3.client('sts',region_name=region)
-
-        # Obtener el ARN del rol
-        role_arn = f'arn:aws:iam::{sts_client.get_caller_identity()["Account"]}:role/{role_name}'
-
-        print(f"ARN del rol: {role_arn}")
-
-        # Asumir el rol para obtener credenciales temporales
-        assumed_role = sts_client.assume_role(
-            RoleArn=role_arn,
-            RoleSessionName='RoleForPresharedURL',
-            DurationSeconds=604800 # 1 semana
-        )
-
-        print(f"Rol asumido correctamente: {assumed_role}")
-
-        # Obtener las credenciales temporales
-        credentials = assumed_role['Credentials']
-
-        print(f"Credenciales temporales obtenidas correctamente: {credentials}")
-
-        # Crear un nuevo cliente S3 con las credenciales temporales
-        s3_client = boto3.client(
-            's3',
-            aws_access_key_id=credentials['AccessKeyId'],
-            aws_secret_access_key=credentials['SecretAccessKey'],
-            aws_session_token=credentials['SessionToken'],
-            region_name=region
-        )
-
-        # Generar URL prefirmada para thumbnail
+        # Generate presigned url for thumbnail
         thumbnail_presigned_url = s3_client.generate_presigned_url(
             'get_object',
             Params={
                 'Bucket': bucket_name,
                 'Key': f"{bucket_key}/bs_thumbnail.png"
             },
-            ExpiresIn=604800 # 1 semana
+            ExpiresIn=604800  # 1 week
         )
-
-        # Generar URL prefirmada para output.zip
+        # Generate presigned url for output.zip
         output_zip_presigned_url = s3_client.generate_presigned_url(
             'get_object',
             Params={
                 'Bucket': bucket_name,
                 'Key': f"{bucket_key}/output.zip"
             },
-            ExpiresIn=604800 # 1 semana
+            ExpiresIn=604800  # 1 week
         )
-
         return thumbnail_presigned_url, output_zip_presigned_url
     except ClientError as e:
         print(f"Error: {e.response['Error']['Code']}")
-        return None, None
+        return None, None 
